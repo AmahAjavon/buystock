eab7e530789d195595e4988a4eab7be178b1e7ce
AmahAjavon <aj_ajavon@hotmail.com>
2015-03-04 18:17:26 -0800

    initial functional, more to come

--- client/main.js
+++ client/main.js
@@ -1,1 +1,66 @@
-console.log('hello, world');
+'use strict';
+
+$(document).ready(init);
+
+var totalPosition = 0;
+var totalAssets = 0;
+var $update;
+var $sell;
+var sum = 0;
+var inp = parseFloat(clickVal);
+var clickVal = $('#cashin').val();
+
+function init() {
+  $('#buy').click(getData);
+  $('#addcash').click(function() {
+    var clickVal = $('#cashin').val();
+    var inp = parseFloat(clickVal);
+    sum = inp + sum;
+    $('#sum').html('Balance is: $'+sum);
+    totalAssets = totalPosition + sum;
+    $('#assets').html('Total Assets is: $' +totalAssets);
+  });
+}
+
+function getData() {
+  var input = $('#symbol').val();
+  var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol='+ input +'&callback=?';
+  $.getJSON(url, function(data) {
+    var $div = $('<div>');
+    $div.addClass('answer');
+    var share = $('#shares').val();
+    var position = parseInt(data.LastPrice) * share;
+
+    if (!input || !isNaN(input)) {
+      alert("Please enter the right symbol");
+    }  if  (totalAssets < position) {
+      alert("You need more money");
+    }
+    else {
+      console.log('data', data);
+      $('#sum').html('Balance is: $'+(sum -= position));
+
+      var $show = $div.html('Symbol: ' + data.Symbol +'<br/>'+ 'Quote: ' + data.LastPrice +'<br/>' + share + ' shares'+'<br/>' + 'Position: $'+ position +'<br/>');
+      $('#messages').append($show);
+
+      $update = $("<input type='button' id='update' value='Update'/>");
+      $sell = $("<input type='button' id='sell' value='Sell'/>");
+      $show.append($update, $sell);
+
+      totalPosition += position;
+      $('#position').html('Total Position is: $' +totalPosition);
+
+      totalAssets = totalPosition + sum;
+      $('#assets').html('Total Assets is: $' +totalAssets);
+    }
+    $('#update').click(function() {
+      console.log('update pressed');
+    });
+    // var $newUpdate = $div.html('Symbol: ' + data.Symbol +'<br/>'+ 'Quote: ' + data.LastPrice +'<br/>' + share + ' shares'+'<br/>' + 'Position: $'+ position +'<br/>');
+    // $newUpdate.append($show);
+    $('#sell').click(function() {
+      console.log('sell pressed');
+    });
+
+  });
+}
