3cd8e461e980ceef1ba3035f6cedb0c2345eb266
AmahAjavon <aj_ajavon@hotmail.com>
2015-03-05 14:52:22 -0800

    Small update fixed addCash conditions

--- client/main.js
+++ client/main.js
@@ -7,21 +7,25 @@ var totalAssets = 0;
 var $update = $('#update');
 var $sell = $('#sell');
 var sum = 0;
-var clickVal = $('#cashin').val();
-var inp = parseFloat(clickVal);
-
+// var clickVal = $('#cashin').val();
+// var inp = parseFloat(clickVal);
 
 function init() {
   $('#buy').click(getData);
   $('#addcash').click(function() {
+
     var clickVal = $('#cashin').val();
     var inp = parseFloat(clickVal);
     sum = inp + sum;
+    if (!clickVal) {
+      alert('Please add cash to your balance');
+    } else {
     $('#sum').html('Balance is: $'+ sum);
     totalAssets = totalPosition + sum;
     $('#assets').html('Total Assets is: $' +totalAssets);
+    }
   });
-}
+ }
 
 function getData() {
   var input = $('#symbol').val();
@@ -33,9 +37,9 @@ function getData() {
     var position = parseInt(data.LastPrice) * share;
 
     if (!input || !isNaN(input)) {
-      alert("Please enter the right symbol");
-    }  if  (totalAssets < position) {
-      alert("You need more money");
+      alert('Please enter the right symbol');
+    }  else if  (totalAssets < position) {
+      alert('You need more money');
     }
     else {
       console.log('data', data);
@@ -55,21 +59,21 @@ function getData() {
       $('#assets').html('Total Assets is: $' +totalAssets);
 
       $update.click(function() {
-
-        $.getJSON(url, function(data) {
+          $.getJSON(url, function(data) {
           var $div = $('<div>');
           $div.addClass('answer');
           var share = $('#shares').val();
           var position = parseInt(data.LastPrice) * share;
           var $show = $div.html('Symbol: ' + data.Symbol +'<br/>'+ 'Quote: ' + data.LastPrice +'<br/>' + share + ' shares'+'<br/>' + 'Position: $'+ position +'<br/>');
           $('#messages').append($show);
+          $update = $("<input type='button' id='update' value='Update'/>");
+          $sell = $("<input type='button' id='sell' value='Sell'/>");
           $show.append($update, $sell);
           $show.remove();
           console.log(data.LastPrice);
         });
       });
-      // var $newUpdate = $div.html('Symbol: ' + data.Symbol +'<br/>'+ 'Quote: ' + data.LastPrice +'<br/>' + share + ' shares'+'<br/>' + 'Position: $'+ position +'<br/>');
-      // $newUpdate.append($show);
+
       $sell.click(function() {
         console.log('sell pressed');
         $('#sum').html('Balance is: $'+ (sum += position));
